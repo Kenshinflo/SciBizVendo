25b0920034406db173c6d8b09511250f62ecbb5e
kenshinflo <gerardkengalindo@gmail.com>
2024-03-19 22:38:44 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+  // import HomeView from './views/HomeView.vue';
+  // import NavView from './views/NavView.vue';
+  import {RouterView} from 'vue-router'
+
+</script>
+
+<template>
+  <main class="bg-teal-300 m-0 p-0">
+    <RouterView />
+  </main>
+</template>
+
+<style >
+  html{
+    max-height: 1280px;
+    max-width: 800px;
+    /* overflow: hidden; */
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import OptionView from '../views/OptionView.vue'
+import StoreView from '../views/StoreView.vue'
+import ModalEx from '../components/BaseModal.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/option',
+      name: 'option',
+      component: OptionView
+    },
+    {
+      path: '/store',
+      name: 'store',
+      component: StoreView
+    },
+    {
+      path: '/modal',
+      name: 'modalex',
+      component: ModalEx
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
